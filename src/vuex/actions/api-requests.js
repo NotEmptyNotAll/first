ef5f69a0f6fd29dace0a3e57cd5b9cad5d2c8872
NotEmptyNotAll <shvetsvlad15@gmail.com>
2020-09-02 20:01:18 +0300

    vlad

--- src/vuex/actions/api-requests.js
+++ src/vuex/actions/api-requests.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 //let urlApi = 'https://enginefinal.herokuapp.com/';
-let urlApi = 'http://localhost:5050/';
+let urlApi = 'http://10.102.0.1:5050/';
 
 export default {
     async GET_AUTO_ENG_PAGINATION({commit}, request) {
