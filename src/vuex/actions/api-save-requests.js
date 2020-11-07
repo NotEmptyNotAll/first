6a197526e215830792cdf0e4ff111c0766ed6339
Vlad <shvetsvlad15@gmail.com>
2020-11-07 16:02:12 +0200

    vlad

--- src/vuex/actions/api-save-requests.js
+++ src/vuex/actions/api-save-requests.js
@@ -3,7 +3,7 @@ import axios from "axios";
 //let urlApi = 'https://enginefinal.herokuapp.com/save/';
 //let urlApi = 'http://10.102.200.11:5050/save/';
 //let urlApi = 'http://eng-test.24x7tools.com:5050/save/';
-//let urlApi = 'http://10.102.200.11:5050/save/';
+let urlApi = 'http://10.102.200.11:5050/save/';
 //let urlApi = 'http://localhost:5050/save/';
 
 export default {
