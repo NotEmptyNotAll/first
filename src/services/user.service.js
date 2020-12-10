912c41b7b312b07cb8e2e54c2f833cd19a657769
Vlad <shvetsvlad15@gmail.com>
2020-12-10 05:44:13 +0200

    vlad

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 import authHeader from './auth-header';
 
 //const API_URL = 'http://10.102.0.1:5050/';
-let API_URL = 'http://eng-test.24x7tools.com:5050/';
+//let API_URL = 'http://eng-test.24x7tools.com:5050/';
 //const API_URL = 'https://newenginedb.herokuapp.com/';
 //const API_URL = 'http://localhost:5050/';
-//const API_URL = 'http://10.102.200.11:5050/';
+const API_URL = 'http://10.102.200.11:5050/';
 
 //user = JSON.parse(localStorage.getItem('user'));
 class UserService {
